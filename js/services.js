81521509f45de60fb3705c80c633978372fab0f8
Alan <gagazcfan@gmail.com>
2012-11-21 21:36:29 -0800

    Add localStorage feature to services.html, you can click the buy buttons to book fakely services now.

---
+++ js/services.js
@@ -0,0 +1,20 @@
+function buy(service) {
+	if (localStorage[service]) {
+		document.getElementById(service).innerHTML = "";
+		localStorage.removeItem(service);
+	} else {
+		document.getElementById(service).innerHTML = "Booked! Click again to cancel.";
+		localStorage[service] = true;
+	}
+}
+
+function init (service) {
+	if (localStorage[service]) {
+		document.getElementById(service).innerHTML = "Booked! Click again to cancel.";
+	}
+}
+
+init("design");
+init("phosting");
+init("chosting");
+init("support");
